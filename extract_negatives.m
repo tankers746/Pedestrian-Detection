8fc8f83224344bf8679ea071e196da5b70811426
tankers746 <tankers746@live.com>
2018-05-20 17:53:37 +0800

    Recropped positive and negative samples

--- extract_negatives.m
+++ extract_negatives.m
@@ -2,6 +2,9 @@
 baseDir = 'C:\Users\Tom\OneDrive\Documents\CITS4402\Pedestrian-Detection\';
 annotDir = [baseDir 'PennFudanPed\Annotation\'];
 
+ratio = 2;
+image_width = 50;
+
 files = dir(annotDir); files(1:2) = [];
 close all;
 for ii = 1 : length(files)
@@ -15,18 +18,22 @@ for ii = 1 : length(files)
         crop_width = bbox(3);
         shift=150;
         
-        right_shift_xmin = bbox(1)+crop_width;
-        if right_shift_xmin + crop_width <= width 
-            neg_right =  [right_shift_xmin, bbox(2), bbox(3), bbox(4)];
-            crop_right = imcrop(I, neg_right);
-            imwrite(crop_right, strcat('C:\negative\', files(ii).name, num2str(jj),'right.png'));
-        end
-        
-        left_shift_xmin = bbox(1)-crop_width;
-        if left_shift_xmin >= 0
-            neg_left = [left_shift_xmin, bbox(2), bbox(3), bbox(4)];
-            crop_left = imcrop(I, neg_left);
-            imwrite(crop_left, strcat('C:\negative\', files(ii).name, num2str(jj),'left.png'));
+        if(crop_width >= image_width)
+            right_shift_xmin = bbox(1)+crop_width;
+            if right_shift_xmin + crop_width <= width 
+                neg_right =  [right_shift_xmin, bbox(2), bbox(3), crop_width*ratio];
+                crop_right = imcrop(I, neg_right);
+                resized = imresize(crop_right, [image_width*ratio, image_width]);
+                imwrite(resized, strcat('C:\negative\', files(ii).name, num2str(jj),'right.png'));
+            end
+
+            left_shift_xmin = bbox(1)-crop_width;
+            if left_shift_xmin >= 0
+                neg_left = [left_shift_xmin, bbox(2), bbox(3), crop_width*ratio];
+                crop_left = imcrop(I, neg_left);
+                resized = imresize(crop_left, [image_width*ratio, image_width]);
+                imwrite(resized, strcat('C:\negative\', files(ii).name, num2str(jj),'left.png'));
+            end
         end
     end
     hold off;    
--- extract_positives.m
+++ extract_positives.m
@@ -2,17 +2,36 @@
 baseDir = 'C:\Users\Tom\OneDrive\Documents\CITS4402\Pedestrian-Detection\';
 annotDir = [baseDir 'PennFudanPed\Annotation\'];
 
+ratio = 2;
+image_width = 50;
+
 files = dir(annotDir); files(1:2) = [];
 close all;
 for ii = 1 : length(files)
     fileName = [annotDir files(ii).name];
     record = PASreadrecord(fileName);
     I = imread([baseDir record.imgname]);
+    [height, width, depth] = size(I);
     for jj = 1 : length(record.objects)
         bbox = record.objects(jj).bbox;
         bbox(3:4) = bbox(3:4) - bbox(1:2);
-        cropped_image = imcrop(I, bbox);
-        imwrite(cropped_image, strcat('C:\cropped\', files(ii).name, num2str(jj),'.png'));
+        if(bbox(3) >= image_width)
+            new_width = max(bbox(3), bbox(4)/ratio);
+            
+            max_height = min(height, new_width*ratio);
+            new_height = max(max_height, bbox(4));
+            
+            new_width = new_height / ratio;
+            height_diff = new_height - bbox(4);
+            width_diff = new_width - bbox(3);
+            bbox(1) = bbox(1) - width_diff / 2;
+            bbox(2) = bbox(2) - height_diff / 2;
+            bbox(3) = new_width;
+            bbox(4) = new_height;
+            cropped_image = imcrop(I, bbox);
+            resized = imresize(cropped_image, [image_width*ratio, image_width]);
+            imwrite(resized, strcat('C:\cropped\', files(ii).name, num2str(jj),'.png'));
+        end
     end
     hold off;    
     pause(0.5);
