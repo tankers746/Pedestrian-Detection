08cf269531164a6d7c830d1d94456cf6e36c2090
vos-uwam <skevos.karpathakis@gmail.com>
2018-06-04 16:13:26 +0800

    hog ok

--- use_HOG_classifier.m
+++ use_HOG_classifier.m
@@ -7,7 +7,7 @@ rgb = imread(ExPath);
 im = rgb2gray(rgb);
 
 [Oy,Ox] = size(im); % get original image size, for later
-scaleFactor = 100/0.8; % tune denominator for pedestrian height as a fraction of image height
+scaleFactor = 100/0.6; % tune denominator for pedestrian height as a fraction of image height
 scale = scaleFactor/Oy;
 im2 = imresize(im,scale);
 [Ny,Nx] = size(im2); % new image sizes
@@ -16,6 +16,7 @@ sx = 50;
 sy = 100;
 overlap = 5;
 % slide
+imshow(im2); hold on
 for hx = 1:overlap:(Nx-sx)
     for hy = 1:overlap:(Ny-sy)
         seg=im2(hy:(hy+sy-1),hx:(hx+sx-1));
@@ -23,7 +24,7 @@ for hx = 1:overlap:(Nx-sx)
         features = extractHOGFeatures(seg);
         [label, score] = predict(classifier,features);
         if label == 'positive'
-            hx, hy
+            rectangle('Position', [hx hy sx sy])
         end
     end
 end
